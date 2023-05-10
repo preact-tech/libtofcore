c0c83719e08903de62fc31cc21ba5ddd6084f751
Matt Schuckmann <matt.schuckmann@preact-tech.com>
2023-05-10 23:12:36 +0000

    Fix up pytests to avoid warnings due to unregisted markers

--- test/python/conftest.py
+++ test/python/conftest.py
@@ -1,7 +1,16 @@
 def pytest_addoption(parser):
-    parser.addoption(
-        '--sensor-port-name', action='store', default=None, help='Device name for connecting to the sensor under test'
-    )
-    parser.addoption(
-        '--sensor-baud-rate', action='store', default=None, help='Baud rate to use when connecting to the sensor under test'
+    try:
+        parser.addoption(
+            '--sensor-port-name', action='store', default=None, help='Device name for connecting to the sensor under test'
+        )
+        parser.addoption(
+            '--sensor-baud-rate', action='store', default=None, help='Baud rate to use when connecting to the sensor under test'
+        )
+    except ValueError:
+        #Ignore ValueErrors due to adding the option more than once in different config files.
+        pass
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "functional: Tests to run with functional sensor connected"
     )
