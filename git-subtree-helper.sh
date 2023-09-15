2b6866dc5120fc194856aaabf00d133081a9b5c7
Adam Kohut <adam.kohut@preact-tech.com>
2023-09-15 09:06:46 -0700

    script change

--- git-subtree-helper.sh
+++ git-subtree-helper.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-SOURCE_BRANCH="libtofcore-source"
-STAGING_BRANCH="libtofcore-staging"
+SOURCE_BRANCH="libtofcore_internal-source"
+STAGING_BRANCH="libtofcore_internal-staging"
 
 echo
-echo "Fetch new changes from libtofcore"
+echo "Fetch new changes from libtofcore_internal"
 echo -----------------------------------------
-git fetch libtofcore develop
+git fetch libtofcore_internal develop
 
-LIBTOFCORE_LATEST_COMMIT=`git ls-remote libtofcore | grep "refs/heads/develop" | awk '{ print $1}'`
+LIBTOFCORE_LATEST_COMMIT=`git ls-remote libtofcore_internal | grep "refs/heads/develop" | awk '{ print $1}'`
 echo
-echo "libtofcore latest commit: ${LIBTOFCORE_LATEST_COMMIT}"
+echo "libtofcore_internal latest commit: ${libtofcore_internal_LATEST_COMMIT}"
 echo
 
 # checkout source repo
-git checkout -b ${SOURCE_BRANCH} libtofcore/develop
+git checkout -b ${SOURCE_BRANCH} libtofcore_internal/develop
 
 ls
 # create new staging branch from all the commits impacting "/my-chart" from source repo
 git subtree split -P tofcore -b ${STAGING_BRANCH}
 
 # checkout develop
-git checkout test
+git checkout main
 
 # take commits in the staging branch and set "/helm-charts/my-chart" as the commit root
 # after you run this script the first time, update the command below to:
@@ -29,7 +29,10 @@ git checkout test
 echo
 echo "Add in changes"
 echo -----------------------------------------
-git subtree add -P test ${STAGING_BRANCH} --message "Update helmchart from https://github.com/icheko/libtofcore/commit/${LIBTOFCORE_LATEST_COMMIT}"
+git subtree add -P tmp ${STAGING_BRANCH} --message "Update helmchart from https://github.com/icheko/libtofcore_internal/commit/${LIBTOFCORE_LATEST_COMMIT}"
+
+mv tmp/* .
+rm -rf tmp
 
 # clean up
 echo
